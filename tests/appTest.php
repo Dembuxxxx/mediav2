b85d611e1c5608619a702814c3233b1ad9eb4358
Dembuxxxx <dems75018@hotmail.fr>
2021-12-01 23:07:05 +0100

    test unitaire fait

---
+++ tests/appTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+
+public function testTestsareWorking () {
+ $this->assertEquals(2, 1+1);
+
+
+  }
+
+
+} 
+//création de la classe et etendre test//
+// création d'une methode function pour savoir si le test fonctionne bien//
+// mettre la methode qui sont appellé insertion et expectation//
